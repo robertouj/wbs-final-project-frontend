e0255e4c11236f7100f9eff478469c48619dbd0d
Dimitrios Akritidis <Jimko25@yahoo.gr>
2021-06-08 12:21:12 +0200

    fey help

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Container, Row, Col, Image } from 'react-bootstrap';
+import { Container, Row, Col, Image, Card } from 'react-bootstrap';
 import Data from "../../MockData.json"
 import './Cards.css'
 
 export default function Cards({ nickname, bio }) {
     return (
         <div>
-            <Container text-xs-start className="mentor--container">
+            {/* <Container text-xs-start className="mentor--container">
                 <Row>
                     <Col><Image roundedCircle src={'https://picsum.photos/200/300'} alt="" style={{width: '7rem'}}/></Col>
                     <Col xs={6} className="mentor--name" >{nickname}</Col>
@@ -15,7 +15,37 @@ export default function Cards({ nickname, bio }) {
                     <Col>1 of 3</Col>
                     <Col xs={6} className="bio--section">{bio}</Col>
                 </Row>
-            </Container>    
+            </Container>     */}
+            <Container >
+                            <Row className="rows mt-3">
+                                <Col className="columns" sm>
+                                    <Card style={{ width: '50rem' }} className="ms-auto">
+                                        <Card.Body>
+                                            <Card.Title className="mb-5 ms-3">Mentor 1</Card.Title>
+                                            <Container>
+                                                <Row>
+                                                    <Col >
+                                                        <Image src="achmed.jpg" width="100rem" roundedCircle />
+                                                        <span>placeholder</span> <br></br>
+                                                        <span>placeholder</span>
+
+                                                    </Col>
+                                                    <Col sm={9}>
+                                                        <Card.Subtitle className="mb-2 text-muted">skills field</Card.Subtitle>
+                                                        <Card.Text>
+                                                            description description description description description description description description description description description description description description description
+                </Card.Text>
+                                                        <Card.Link href="#">Card Link</Card.Link>
+                                                        <Card.Link href="#">Another Link</Card.Link>
+                                                    </Col>
+                                                </Row>
+                                            </Container>
+
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            </Row>
+            </Container>
         </div>
     )
 }
