99ee9c3c7c763f1f34f0aa96cb603d28fca22e3c
Saqer <ahmadsaqer95@outloo.com>
2021-06-09 17:44:32 +0200

    fix button

--- src/components/profile/BreadCrumbNav.js
+++ src/components/profile/BreadCrumbNav.js
@@ -1,5 +1,5 @@
 import { Link, Switch, Route } from 'react-router-dom'
-import { Breadcrumb, Container, Row, Col } from 'react-bootstrap';
+import { Breadcrumb, Container, Row, Col, Button } from 'react-bootstrap';
 import Personal from "./Personal"
 import Bio from "./Bio"
 import Skills from "./Skills"
@@ -9,36 +9,37 @@ import "./BreadCrumNav.css"
 
 const BreadCrumbNav = ({ me }) =>
 {
-  const NewTab = () => {
-    window.open(`/#/sessions/c9f2a12622782`, "_blank");
-  };
+    const NewTab = () =>
+    {
+        window.open(`/#/sessions/c9f2a12622782`, "_blank");
+    };
     return (
-    <>
-      <Container className="my-5 py-5">
-        <Row>
-          <Col
-            lg={{ span: 12, offset: 4 }}
-            md={{ span: 12, offset: 3 }}
-            sm={{ span: 12, offset: 1 }}
-            xs={{ span: 12, offset: 1 }}
-          >
-            <Breadcrumb>
-              <Breadcrumb.Item>
-                <Link to="/profile/personal">Personal-details</Link>
-              </Breadcrumb.Item>
-              <Breadcrumb.Item>
-                <Link to="/profile/bio">Bio</Link>
-              </Breadcrumb.Item>
-              <Breadcrumb.Item>
-                <Link to="/profile/skills">Skills</Link>
-              </Breadcrumb.Item>
-              <Breadcrumb.Item>
-                <Link to="/profile/schedule">Schedules</Link>
-              </Breadcrumb.Item>
-              <Breadcrumb.Item>
-                <Link to="/profile/wallet">Wallet</Link>
-              </Breadcrumb.Item>
-            </Breadcrumb>
+        <>
+            <Container className="my-5 py-5">
+                <Row>
+                    <Col
+                        lg={{ span: 12, offset: 4 }}
+                        md={{ span: 12, offset: 3 }}
+                        sm={{ span: 12, offset: 1 }}
+                        xs={{ span: 12, offset: 1 }}
+                    >
+                        <Breadcrumb>
+                            <Breadcrumb.Item>
+                                <Link to="/profile/personal">Personal-details</Link>
+                            </Breadcrumb.Item>
+                            <Breadcrumb.Item>
+                                <Link to="/profile/bio">Bio</Link>
+                            </Breadcrumb.Item>
+                            <Breadcrumb.Item>
+                                <Link to="/profile/skills">Skills</Link>
+                            </Breadcrumb.Item>
+                            <Breadcrumb.Item>
+                                <Link to="/profile/schedule">Schedules</Link>
+                            </Breadcrumb.Item>
+                            <Breadcrumb.Item>
+                                <Link to="/profile/wallet">Wallet</Link>
+                            </Breadcrumb.Item>
+                        </Breadcrumb>
                     </Col>
                 </Row>
             </Container>
@@ -54,11 +55,11 @@ const BreadCrumbNav = ({ me }) =>
 
                 </Route>
                 <Route path="/profile/Schedule">
-                <Button
-            variant="primary"
-            onClick={NewTab}
-          >
-            <i className="bi bi-door-open-fill"></i> Enter room
+                    <Button
+                        variant="primary"
+                        onClick={NewTab}
+                    >
+                        <i className="bi bi-door-open-fill"></i> Enter room
           </Button>
                     <Schedule />
 
