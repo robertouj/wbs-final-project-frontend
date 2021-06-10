b4436aaf8a526b500abcd6bb3816e8bfd548b2f1
Dimitrios Akritidis <Jimko25@yahoo.gr>
2021-06-10 19:09:11 +0200

    About me section style

--- src/components/profile/Personal.js
+++ src/components/profile/Personal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Image } from "react-bootstrap";
 import "./Personal.css"
 
 export default function Personal() {
@@ -9,19 +9,20 @@ export default function Personal() {
       <Container className="personal--container">
         <Row>
           <Col className="d-flex justify-content-center">
-            <p>Fill the information related to your user profile.</p>
+            {/* <p>Fill the information related to your user profile.</p> */}
             <form>
               <div className="control">
                 <h3>Account details</h3>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis, asperiores.</p>
                 <label for="foro">Your avatar</label>
+                <Image className="personal--img" src="https://images.pexels.com/photos/1933873/pexels-photo-1933873.jpeg?cs=srgb&dl=pexels-thgusstavo-santana-1933873.jpg&fm=jpg"/>
                 <input
                   type="file"
                   className="name"
                   required
                   placeholder="send a photo..."
                   />
-                <label for="name">How other see your name</label>
+                <label for="name">Username</label>
                 <input
                   type="text"
                   className="name"
@@ -31,7 +32,7 @@ export default function Personal() {
                 />
               </div>
               <div className="control">
-                <label for="name">Your user and contact e-mail</label>
+                <label for="name">Email</label>
                 <input
                   type="email"
                   className="email"
@@ -41,7 +42,7 @@ export default function Personal() {
                 />
               </div>
               <h3>Bio</h3>
-              <p>
+              <p className="personal--bio">
                 Other people want to know your motivations, your inspirations,
                 what you can offer to do the world a better place. Feel free to
                 share that you want, or simply say hello. ;)
@@ -49,7 +50,7 @@ export default function Personal() {
               <div className="control">
                 <label for="name">Tell your own story</label>
                 <textarea
-                  className="email"
+                  className="email "
                   value="To sorry world an at do spoil along. Incommode he depending do frankness remainder to. Edward day almost active him friend thirty piqued. People as period twenty my extent as. Set was better abroad ham plenty secure had horses. Admiration has sir decisively excellence say everything inhabiting acceptance. Sooner settle add put you sudden him. "
                 />
               </div>
