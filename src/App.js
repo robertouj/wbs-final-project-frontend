e143889b00f063960c5a4d742ccff2eba8d2254c
Roberto Ubieto Juan <robertouj@gmail.com>
2021-07-28 12:16:24 +0200

    profile restricted and hide for unlogged users

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
-import './App.css';
-import React from 'react'
-import { useState } from 'react'
-import SearchBar from './components/SearchBar'
-import CardList from "./components/Cards/CardList"
-import MainNavBar from './components/MainNavBar'
+import "./App.css";
+import React from "react";
+import { useState } from "react";
+import { Route, Switch } from "react-router-dom";
+import Profile from "./components/profile/Profile";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import About from "./components/About";
+import SearchBar from "./components/SearchBar";
+import CardList from "./components/Cards/CardList";
+import MainNavBar from "./components/MainNavBar";
+import ProtectedRoute from "./components/ProtectedRoute";
 
+const APIURL = "https://wbs-final-json-api.herokuapp.com/";
 
-
-const APIURL = 'https://wbs-final-json-api.herokuapp.com/';
-
-
-
-
-
-function App()
-{
-
-
+function App() {
   return (
     <>
-
       <MainNavBar />
-
+      <Switch>
+        <Route path="/" exact>
+          <SearchBar />
+        </Route>
+        <Route path="/About">
+          <About />
+        </Route>
+        <ProtectedRoute path="/profile">
+          <Profile />
+        </ProtectedRoute>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
     </>
-
   );
 }
 
--- src/components/MainNavBar.js
+++ src/components/MainNavBar.js
@@ -1,73 +1,64 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Navbar, Nav, Image } from "react-bootstrap";
-import { Link, Route, Switch } from "react-router-dom";
-import Profile from "./profile/Profile";
-import Register from "./Register";
-import Login from "./Login";
-import SearchBar from "./SearchBar";
-import logo from '../img/logo1.png'
-import About from "./About";
-import "./MainNavBar.css"
+import { Link } from "react-router-dom";
+import logo from "../img/logo1.png";
+import "./MainNavBar.css";
+import { AuthContext } from "../AuthContext";
 
-export default function MainNavBar()
-{
-
+export default function MainNavBar() {
+  const { isLoggedIn, logout } = useContext(AuthContext);
 
   return (
     <div>
       <Navbar style={{ backgroundColor: "#ac66cc" }} expand="sm">
-        <Navbar.Brand className="ms-5" href="/" style={{ color: '#ffd8cc' }}>
+        <Navbar.Brand className="ms-5" href="/" style={{ color: "#ffd8cc" }}>
           <Image src={logo} width="200rem" rounded />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto me-4 justify-content-between navbar--links" >
+          <Nav className="ms-auto me-4 justify-content-between navbar--links">
             <Nav.Link>
-              <Link style={{ textDecoration: "none", color: '#ffd8cc' }} to="/">
+              <Link style={{ textDecoration: "none", color: "#ffd8cc" }} to="/">
                 Home
               </Link>
             </Nav.Link>
             <Nav.Link>
-              <Link style={{ textDecoration: "none", color: '#ffd8cc' }} to="/About">
+              <Link
+                style={{ textDecoration: "none", color: "#ffd8cc" }}
+                to="/About"
+              >
                 About
               </Link>
             </Nav.Link>
             <Nav.Link>
-              <Link style={{ textDecoration: "none", color: '#ffd8cc' }} to="/profile/about-me">
-                Profile
-              </Link>
+              {isLoggedIn() && (
+                <Link
+                  style={{ textDecoration: "none", color: "#ffd8cc" }}
+                  to="/profile/about-me"
+                >
+                  Profile
+                </Link>
+              )}
             </Nav.Link>
             <Nav.Link>
-              <Link style={{ textDecoration: "none", color: '#ffd8cc' }} to="/login">
+              <Link
+                style={{ textDecoration: "none", color: "#ffd8cc" }}
+                to="/login"
+              >
                 Login
               </Link>
             </Nav.Link>
             <Nav.Link>
-              <Link style={{ textDecoration: "none", color: '#ffd8cc' }} to="/register">
+              <Link
+                style={{ textDecoration: "none", color: "#ffd8cc" }}
+                to="/register"
+              >
                 Register
               </Link>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-
-      <Switch>
-        <Route path="/" exact>
-          <SearchBar />
-        </Route>
-        <Route path="/About">
-          <About />
-        </Route>
-        <Route path="/profile">
-          <Profile />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
-      </Switch>
     </div>
   );
 }
